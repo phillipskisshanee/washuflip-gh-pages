bdcc67acab61292f39af094755bc867c662acaa5
phillipskisshanee <phillips.kisshanee@wustl.edu>
2016-04-14 00:52:22 -0500

    Create carousel.js

---
+++ main/js/carousel.js
@@ -0,0 +1,8 @@
+    <!-- Carousel -->
+    <script language="JavaScript" type="text/javascript">
+  $(document).ready(function(){
+    $('.carousel').carousel({
+      interval: 2000
+    })
+  });    
+</script>
